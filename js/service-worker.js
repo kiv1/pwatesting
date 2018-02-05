1c00411241e85a5d4e47db985af83dfe56da9ad4
Mohd Noorjasah <zmohdnoorjasah@kpmg.com.sg>
2018-02-05 16:34:22 +0800

    cache ui

---
+++ js/service-worker.js
@@ -0,0 +1,18 @@
+var cacheName = 'pwatesting';
+var filesToCache = [ '/index.html',
+  '/js/index.js',
+  '/css/style.css'];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
