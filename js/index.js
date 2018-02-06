95b53bb9c8bd4e0e09237ec870826b9a9715eb59
Mohd Noorjasah <zmohdnoorjasah@kpmg.com.sg>
2018-02-06 09:52:22 +0800

    added promises

--- js/index.js
+++ js/index.js
@@ -27,7 +27,10 @@ document.querySelector('#Login').addEventListener('click', function() {
     sendTestForm({
       command: 'add',
       data: data
-    }) // If the promise rejects, show the error.
+    }).then(function() {
+      // If the promise resolves, just display a success message.
+      console.log("Added to cache");
+    }).catch(console.log("err"));  // If the promise rejects, show the error.
 });
 
 
@@ -39,21 +42,29 @@ document.querySelector('#sync').addEventListener('click', function() {
         data.data.forEach(function(url) {
           console.log(url);
         });
-      }) // If the promise rejects, show the error.
+      }).catch(console.log("err")); // If the promise rejects, show the error.
 });
 
 
 function sendTestForm(message){
   console.log(message.data);
-  var messageChannel = new MessageChannel();
-  messageChannel.port1.onmessage = function(event) {
-    if (event.data.error) {
-      console.log(event.data.error);
-    } else {
-      console.log(event.data);
-    }
-  };
-  navigator.serviceWorker.controller.postMessage(message,[messageChannel.port2]);
+  return new Promise(function(resolve, reject) {
+    var messageChannel = new MessageChannel();
+    messageChannel.port1.onmessage = function(event) {
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+
+    // This sends the message data as well as transferring messageChannel.port2 to the service worker.
+    // The service worker can then use the transferred port to reply via postMessage(), which
+    // will in turn trigger the onmessage handler on messageChannel.port1.
+    // See https://html.spec.whatwg.org/multipage/workers.html#dom-worker-postmessage
+    navigator.serviceWorker.controller.postMessage(message,
+      [messageChannel.port2]);
+  });
 }
 
 function getFormData($form){
