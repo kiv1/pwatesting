34e488b9bc716ff18f12914a54e661ed4f7fd5e7
Mohd Noorjasah <zmohdnoorjasah@kpmg.com.sg>
2018-02-06 11:58:49 +0800

    modify indexdDB

--- sw.js
+++ sw.js
@@ -82,15 +82,11 @@ function createDB(){
     // Create an objectStore to hold information about our customers. We're
     // going to use "ssn" as our key path because it's guaranteed to be
     // unique - or at least that's what I was told during the kickoff meeting.
-    var objectStore = db.createObjectStore("DataStore", { keyPath: "ssn" });
+    var objectStore = db.createObjectStore("DataStore", { keyPath: "ID" });
 
     // Create an index to search customers by name. We may have duplicates
     // so we can't use a unique index.
     objectStore.createIndex("JSON", "JSON", { unique: false });
-
-    // Create an index to search customers by email. We want to ensure that
-    // no two customers have the same email, so use a unique index.
-    objectStore.createIndex("ID", "ID", { unique: true });
 
     // Use transaction oncomplete to make sure the objectStore creation is 
     // finished before adding data into it.
