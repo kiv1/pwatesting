436700fcb54af951dec7ca9515bc5cb5c26271e1
Mohd Noorjasah <zmohdnoorjasah@kpmg.com.sg>
2018-02-06 09:28:34 +0800

    added new codes for service worker

--- sw.js
+++ sw.js
@@ -14,9 +14,49 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+//Stuff
+self.clients.matchAll().then(function(clients) {
+  clients.forEach(function(client) {
+    console.log(client);
+    client.postMessage('The service worker just started up.');
+  });
 });
+
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // While there is only one cache in this example, the same logic will handle the case where
+  // there are multiple versioned caches.
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      return clients.claim();
+    }).then(function() {
+      // After the activation and claiming is complete, send a message to each of the controlled
+      // pages letting it know that it's active.
+      // This will trigger navigator.serviceWorker.onmessage in each client.
+      return self.clients.matchAll().then(function(clients) {
+        return Promise.all(clients.map(function(client) {
+          return client.postMessage('The service worker has activated and ' +
+            'taken control.');
+        }));
+      });
+    })
+  );
+});
+
 
 self.addEventListener('message', function(event) {
   console.log('Handling message event:', event);
